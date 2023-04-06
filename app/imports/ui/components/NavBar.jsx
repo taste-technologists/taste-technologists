ff51fe20290dcea13fa793c2e759006d8e490f81
clyyoung <clyyoung@hawaii.edu>
2023-04-05 20:35:14 -1000

    implemented navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
 import { Roles } from 'meteor/alanning:roles';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Image, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 
 const NavBar = () => {
@@ -16,18 +16,25 @@ const NavBar = () => {
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={NavLink} to="/">
-          <h2>meteor-application-template-react</h2>
+          <Image src="/images/taste-technologists-logo.png" width="180px" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
             {currentUser ? ([
-              <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">List Stuff</Nav.Link>,
+              <Nav.Link id="list-recipes-nav" as={NavLink} to="/list" key="add">List Recipes</Nav.Link>,
+              <Nav.Link id="search-recipes-nav" as={NavLink} to="/search" key="list">Search Recipes</Nav.Link>,
+              <Nav.Link id="my-recipes-nav" as={NavLink} to="/my-recipes" key="my-recipes">My Recipes</Nav.Link>,
             ]) : ''}
             {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
               <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
             ) : ''}
+            {Roles.userIsInRole(Meteor.userId(), 'vendor') ? (
+              <Nav.Link id="inventory-vendor-nav" as={NavLink} to="/inventory" key="vendor">Inventory</Nav.Link>
+            ) : ''}
+            {currentUser ? ([
+              <Nav.Link id="profile-nav" as={NavLink} to="/profile" key="profile">Profile</Nav.Link>,
+            ]) : ''}
           </Nav>
           <Nav className="justify-content-end">
             {currentUser === '' ? (
