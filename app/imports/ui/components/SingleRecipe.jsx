c15ac26df5c6de4247c2d95ef46974ba790a9ef1
mendechris <iluvbooks86@gmail.com>
2023-04-07 10:56:31 -1000

    Created SingleRecipe Component
    Component should be used for the individual recipe pages.

---
+++ app/imports/ui/components/SingleRecipe.jsx
@@ -0,0 +1,43 @@
+/* Component for layout out a Profile Card. */
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const SingleRecipeCard = ({ recipe }) => (
+  <Container>
+    <Row className="flex-row justify-content-center">
+      {recipe.description}
+    </Row>
+    <Row>
+      <Col className="text-center"><Image src={recipe.picture} width={400} /></Col>
+    </Row>
+    <Row>
+      <h2>Ingredients</h2>
+      <ul>
+        {recipe.ingredients.map((ing) => <li>{ing}</li>)}
+      </ul>
+    </Row>
+    <Row>
+      <h2>Instructions:</h2>
+      <ol>
+        {recipe.instructions.map((ing) => <li>{ing}</li>)}
+      </ol>
+
+    </Row>
+  </Container>
+);
+
+SingleRecipeCard.propTypes = {
+  recipe: PropTypes.shape({
+    picture: PropTypes.string,
+    name: PropTypes.string,
+    time: PropTypes.string,
+    description: PropTypes.string,
+    ingredients: PropTypes.string,
+    instructions: PropTypes.arrayOf(PropTypes.string),
+    tags: PropTypes.arrayOf(PropTypes.string),
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default SingleRecipeCard;
