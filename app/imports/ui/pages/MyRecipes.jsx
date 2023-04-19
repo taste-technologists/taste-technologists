1304d9cc9007773b045c3bfcc5934d32d47a382f
mendechris <iluvbooks86@gmail.com>
2023-04-18 15:57:44 -1000

    Add functionality where page only displays the user's own recipes or a prompt to add recipes.

--- app/imports/ui/pages/MyRecipes.jsx
+++ app/imports/ui/pages/MyRecipes.jsx
@@ -9,6 +9,7 @@ import LoadingSpinner from '../components/LoadingSpinner';
 
 /* Renders a table containing all of the Recipe documents. Use <RecipeCard> to render each recipe card. */
 const MyRecipesPage = () => {
+  const userName = Meteor.user().username;
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, recipes } = useTracker(() => {
 
@@ -16,17 +17,26 @@ const MyRecipesPage = () => {
     const subscription = Meteor.subscribe(Recipes.generalPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
-    // Get the Recipes
-    const recipeItem = Recipes.collection.find({}).fetch();
+    // Get the Recipes for the logged in user.
+    const recipeItem = Recipes.collection.find({ owner: userName }).fetch();
+
     return {
       recipes: recipeItem,
       ready: rdy,
     };
   }, []);
+  // Get the size of the user's recipe array.
+  // If user has no recipes, the page will display this and
+  // prompt user to add their own recipe.
+  const haveRecipes = recipes.length > 0;
   return (ready ? (
     <Container style={pageStyle}>
       <Row xs={1} md={2} lg={4} className="g-2">
         {recipes.map((recipe) => <RecipeCard key={recipe._id} recipe={recipe} />)}
+      </Row>
+      <Row hidden={haveRecipes} className="text-center pt-5">
+        <h2>You have no recipes!</h2>
+        <h3><a href="../add">Click here to add a recipe.</a></h3>
       </Row>
     </Container>
   ) : <LoadingSpinner />);
