4daacb8f721f631c31720359eb89e5bd5dc58a5b
iwalanicampbell <iwalaniy@hawaii.edu>
2023-04-20 22:52:00 -1000

    Seperate Vendors now being able to see

--- app/imports/ui/pages/IndividualVendorInventory.jsx
+++ app/imports/ui/pages/IndividualVendorInventory.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Col, Container, Row, Table } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import { Inventory } from '../../api/vendor/VendorInventory';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Vendor } from '../../api/vendor/Vendors';
 import Ingredient from '../components/Ingredient';
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 
-const IndividualInventoryView = (vendorID) => {
+const IndividualInventoryView = () => {
+  const vendorID = useParams();
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ingredients, ready } = useTracker(() => {
     // Note that this subscription will get cleaned up
@@ -18,9 +20,11 @@ const IndividualInventoryView = (vendorID) => {
     const subscription2 = Meteor.subscribe(Vendor.generalPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready() && subscription2.ready();
-    // Get the Stuff documents
+    // vendor = Vendor.collection.findOne(vendorID); is only returning Nijiya?
+    // eslint-disable-next-line no-param-reassign
     const vendor = Vendor.collection.findOne(vendorID);
     console.log(vendor);
+    console.log(vendorID);
     const allIngredients = Inventory.collection.find().fetch();
     const vendorIngredients = allIngredients.filter(ingredient => ingredient.name.includes(vendor.name));
     return {
