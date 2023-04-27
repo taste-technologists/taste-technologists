d75be1aad22407b692256bd3350f9e67bdf650d6
mendechris <iluvbooks86@gmail.com>
2023-04-27 09:41:14 -1000

    fixed lint error

--- app/imports/ui/components/SingleRecipe.jsx
+++ app/imports/ui/components/SingleRecipe.jsx
@@ -9,7 +9,6 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { Profiles } from '../../api/profiles/Profiles';
 import LoadingSpinner from './LoadingSpinner';
 import { Inventory } from '../../api/vendor/VendorInventory';
-import Footer from './Footer';
 
 const SingleRecipeCard = ({ recipe }) => {
   const { ready, userProfile, inventory } = useTracker(() => {
