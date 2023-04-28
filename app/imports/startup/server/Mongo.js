8b449708d2fdd0fb5ae5d3b8aa3ddc8f6c37e0d2
iwalanicampbell <iwalaniy@hawaii.edu>
2023-04-27 14:47:59 -1000

    bracket error fixed

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -33,36 +33,37 @@ if (Recipes.collection.find().count() === 0) {
     console.log('Creating default recipes.');
     Meteor.settings.defaultRecipes.forEach(data => addRecipe(data));
   }
-  // Initialize the database with a default inventory document.
-  const addItem = (data) => {
-    console.log(`  Adding: ${data.name} (${data.item})`);
-    Inventory.collection.insert(data);
-  };
+}
+// Initialize the database with a default inventory document.
+const addItem = (data) => {
+  console.log(`  Adding: ${data.name} (${data.item})`);
+  Inventory.collection.insert(data);
+};
 
-  // Initialize the InventoryCollection if empty.
-  if (Inventory.collection.find().count() === 0) {
-    if (Meteor.settings.defaultItem) {
-      console.log('Creating default inventory.');
-      Meteor.settings.defaultItem.forEach(data => addItem(data));
-    }
+// Initialize the InventoryCollection if empty.
+if (Inventory.collection.find().count() === 0) {
+  if (Meteor.settings.defaultItem) {
+    console.log('Creating default inventory.');
+    Meteor.settings.defaultItem.forEach(data => addItem(data));
+  }
+}
+
+// Initialize the database with a default inventory document.
+const addVendor = (data) => {
+  console.log(`  Adding: ${data.name} (${data.hours})`);
+  Vendor.collection.insert(data);
+};
+
+// Initialize the VendorCollection if empty.
+if (Vendor.collection.find().count() === 0) {
+  if (Meteor.settings.defaultVendor) {
+    console.log('Creating default vendors.');
+    Meteor.settings.defaultVendor.forEach(data => addVendor(data));
   }
 
-  // Initialize the database with a default inventory document.
-  const addVendor = (data) => {
-    console.log(`  Adding: ${data.name} (${data.hours})`);
-    Vendor.collection.insert(data);
-  };
+}
 
-  // Initialize the VendorCollection if empty.
-  if (Vendor.collection.find().count() === 0) {
-    if (Meteor.settings.defaultVendor) {
-      console.log('Creating default vendors.');
-      Meteor.settings.defaultVendor.forEach(data => addVendor(data));
-    }
-
-  }
-
-  /**
+/**
    * If the loadAssetsFile field in settings.development.json is true, then load the data in private/data.json.
    * This approach allows you to initialize your system with large amounts of data.
    * Note that settings.development.json is limited to 64,000 characters.
@@ -70,12 +71,11 @@ if (Recipes.collection.find().count() === 0) {
    * For more info on assets, see https://docs.meteor.com/api/assets.html
    * User count check is to make sure we don't load the file twice, which would generate errors due to duplicate info.
    */
-  if ((Meteor.settings.loadAssetsFile) && (Recipes.collection.find().count() < 4)) {
-    const assetsFileName = 'data.json';
-    console.log(`Loading data from private/${assetsFileName}`);
-    // eslint-disable-next-line no-undef
-    const jsonData = JSON.parse(Assets.getText(assetsFileName));
-    jsonData.groceries.map(data => addItem(data));
-    jsonData.recipes.map(data => addRecipe(data));
-  }
+if ((Meteor.settings.loadAssetsFile) && (Recipes.collection.find().count() < 4)) {
+  const assetsFileName = 'data.json';
+  console.log(`Loading data from private/${assetsFileName}`);
+  // eslint-disable-next-line no-undef
+  const jsonData = JSON.parse(Assets.getText(assetsFileName));
+  jsonData.groceries.map(data => addItem(data));
+  jsonData.recipes.map(data => addRecipe(data));
 }
